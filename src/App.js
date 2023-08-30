c209943bfcfd39522d16f034b12149bbcbbb16e6
ninajairacagomoc <jairalael101@gmail.com>
2023-08-30 22:19:18 +0800

    feat: added the navigator for the application

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <TodoList />
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="nav-bar">
+        <nav>
+          <ul>
+            <li><a href={'/'}>Home </a></li>
+            <li><a href={'/done'}>Done List </a></li>
+            <li><a href={'/help'}>Help </a></li>
+          </ul>
+        </nav>
+      </div>
+      <TodoList></TodoList>
+    </div>
+  );
 }
 
 export default App;
